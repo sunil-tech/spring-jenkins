2badf0d559dc4f83abcc438c9a4a258fe9d5609d
unknown <sunilbhardwaj22992@gmail.com>
2024-01-12 01:19:46 +0530

    first message

---
+++ src/test/java/com/example/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	
+	@Test
+	void contextLoads() {
+		logger.info("test case executed");
+		assertEquals(true, true);
+		
+	}
+
+}
